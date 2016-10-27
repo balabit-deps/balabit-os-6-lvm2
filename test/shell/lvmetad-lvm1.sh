27951fe043f6ee4d41dfb0441008312e3b0347bd
testbot <testbot@balabit.hu>
2016-10-27 18:26:38 +0200

    Imported Upstream version 2.02.133

---
+++ test/shell/lvmetad-lvm1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Copyright (C) 2012 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+SKIP_WITHOUT_LVMETAD=1
+SKIP_WITH_LVMPOLLD=1
+
+. lib/inittest
+
+# lvmetad does not support lvm1 format
+
+aux prepare_devs 2
+pvcreate --metadatatype 1 "$dev1"
+not vgscan --cache
+pvs | tee out
+not grep "$dev1" out
+vgcreate --metadatatype 1 $vg1 "$dev1"
+not vgscan --cache
+vgs | tee out
+not grep $vg1 out
+pvs | tee out
+not grep "$dev1" out
+
+# check for RHBZ 1080189 -- SEGV in lvremove/vgremove
+pvcreate -ff -y --metadatatype 1 "$dev1" "$dev2"
+vgcreate --metadatatype 1 $vg1 "$dev1" "$dev2"
+lvcreate -l1 $vg1
+pvremove -ff -y "$dev2"
+vgchange -an $vg1
+not lvremove $vg1
+not vgremove -ff -y $vg1
